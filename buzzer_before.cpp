a2f7e74314270f7a6035c7ffe79986f3d10199f0
Dennis Ahn <unirobotics@AHNDONGui-MacBook-Pro.local>
2018-06-28 22:09:20 -0700

    Code Review

--- buzzer_before.cpp
+++ buzzer_before.cpp
@@ -5,6 +5,10 @@
 * Description           : 
 * Example               : 
 *******************************************************************************/
+
+// 주석
+
+// 함수명과 인수들이 어떤 역할을 하는지로 이름 변경
 void ApiBuzzer(unsigned int type, unsigned int cnt, unsigned int on, unsigned int off)
 {
     struct buzzer_param buzzer;
@@ -18,23 +22,29 @@ void ApiBuzzer(unsigned int type, unsigned int cnt, unsigned int on, unsigned in
     {
         return;
     }
-    usleep((on+10)*1000);
+    usleep((on+10)*1000);   // Magic Number 를 디파인 해주세요.
 }
 
+
+// 함수명 을 Camel Case, UnderBar 통일
+// BYTE 입력을 Enum 처리
+// Legacy 코드로 될 확률이 높아요.  인터페이스를 명확하게 해주세요.
 
 void dev_BEEP_play_sound (BYTE nBeeptype)
 {
     unsigned int type=0;
     
     if ( use_beep == FALSE )
-        return ;    
-        
+        return ;          // Return 문 말고 블럭으로 처리해주세요
+
+    // 인덴트
+    
     switch( nBeeptype)
     {
-    case BEEP_NO:
+    case BEEP_NO:                   // break 처리  // indent
         return ;
     case BEEP_OK:
-        ApiBuzzer( 1, 2, 80, 5);
+        ApiBuzzer( 1, 2, 80, 5);    // 각각의 항목의 의미
         break;
     case BEEP_ERROR:
         ApiBuzzer( 7, 2, 100, 10);
